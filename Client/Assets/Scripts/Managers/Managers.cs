2385b8ff7fea8b216769daf121f9fda1c2ec758c
DeukYu <ldy8070@naver.com>
2022-05-28 12:16:18 +0900

    Managers 만들기
    Managers 만들기(Singleton패턴)

---
+++ Client/Assets/Scripts/Managers/Managers.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    static Managers s_instance;   // 유일성이 보장된다.
+    public static Managers Instance { get{ Init(); return s_instance; } }  // 유일한 매니저를 가지고 온다.
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    static void Init()
+    {
+        if (s_instance == null)
+        {
+            GameObject go = GameObject.Find("@Managers");
+            if(go != null)
+            {
+                go = new GameObject { name = "@Managers" };
+                go.AddComponent<Managers>();
+            }
+
+            DontDestroyOnLoad(go);
+            s_instance = go.GetComponent<Managers>();
+        }
+        
+    }
+}
