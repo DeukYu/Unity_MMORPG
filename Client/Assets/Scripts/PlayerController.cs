2c55355abafdc6e556513ef9ee45d28a347e4c76
DeukYu <ldy8070@naver.com>
2022-05-28 16:30:07 +0900

    Player 움직이기
    Player 움직이기

---
+++ Client/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float _speed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKey(KeyCode.W))
+            transform.position += Vector3.forward * Time.deltaTime * _speed;
+        if (Input.GetKey(KeyCode.S))
+            transform.position -= Vector3.back * Time.deltaTime * _speed;
+        if(Input.GetKey(KeyCode.A))
+            transform.position -= Vector3.left * Time.deltaTime * _speed;
+        if (Input.GetKey(KeyCode.D))
+            transform.position += Vector3.right * Time.deltaTime * _speed;
+    }
+}
