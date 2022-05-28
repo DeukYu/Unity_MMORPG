167f6152219ab7d2f0d66db5f50c7851cb95cf79
DeukYu <ldy8070@naver.com>
2022-05-29 01:38:09 +0900

    Player Position, Rotation
    Player Position, Rotation

--- Client/Assets/Scripts/PlayerController.cs
+++ Client/Assets/Scripts/PlayerController.cs
@@ -12,17 +12,38 @@ public class PlayerController : MonoBehaviour
         
     }
 
+    float _yAngle = 0.0f;
     // Update is called once per frame
     void Update()
     {
-        
+        _yAngle += Time.deltaTime * 100.0f;
+
+        //transform.rotation = Quaternion.Euler(new Vector3(0.0f, _yAngle, 0.0f));
+
         if (Input.GetKey(KeyCode.W))
+        {
+            //transform.rotation = Quaternion.LookRotation(Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.2f);
             transform.position += Vector3.forward * Time.deltaTime * _speed;
+        }
         if (Input.GetKey(KeyCode.S))
-            transform.position -= Vector3.back * Time.deltaTime * _speed;
-        if(Input.GetKey(KeyCode.A))
-            transform.position -= Vector3.left * Time.deltaTime * _speed;
+        {
+            //transform.rotation = Quaternion.LookRotation(Vector3.back);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.2f);
+            transform.position += Vector3.back * Time.deltaTime * _speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            //transform.rotation = Quaternion.LookRotation(Vector3.left);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.2f);
+            transform.position += Vector3.left * Time.deltaTime * _speed;
+        }
+           
         if (Input.GetKey(KeyCode.D))
+        {
+            //transform.rotation = Quaternion.LookRotation(Vector3.right);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
             transform.position += Vector3.right * Time.deltaTime * _speed;
+        }
     }
 }
