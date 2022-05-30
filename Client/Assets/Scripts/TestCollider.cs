2ec05a9e0c8ea5a14f916bf5bec6ee5823e02642
DeukYu <ldy8070@naver.com>
2022-05-31 08:37:25 +0900

    Raycasting, LayerMask
    Raycasting, LayerMask

--- Client/Assets/Scripts/TestCollider.cs
+++ Client/Assets/Scripts/TestCollider.cs
@@ -6,12 +6,12 @@ public class TestCollider : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Collision");
+        Debug.Log($"Collision @{collision.gameObject.name}");
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Trigger");
+        Debug.Log($"Trigger @{other.gameObject.name}");
     }
     // Start is called before the first frame update
     void Start()
@@ -22,6 +22,38 @@ public class TestCollider : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Debug.Log(Input.mousePosition); // Screen
+        //Debug.Log(Camera.main.ScreenToViewportPoint(Input.mousePosition));
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
+
+            LayerMask mask = LayerMask.GetMask("Monster");
+            //int mask = (1 << 8) | (1 << 9);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100.0f, mask))
+            {
+                Debug.Log($"Raycast Camera @ {hit.collider.gameObject.tag}");
+            }
+        }
+
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+        //    Vector3 dir = mousePos - Camera.main.transform.position;
+        //    dir = dir.normalized;
+
+        //    Debug.DrawRay(Camera.main.transform.position, dir * 100.0f, Color.red, 1.0f);
+
+        //    RaycastHit hit;
+        //    if (Physics.Raycast(Camera.main.transform.position, dir, out hit, 100.0f))
+        //    {
+        //        Debug.Log($"Raycast Camera @ {hit.collider.gameObject.name}");
+        //    }
+        //}
     }
 }
