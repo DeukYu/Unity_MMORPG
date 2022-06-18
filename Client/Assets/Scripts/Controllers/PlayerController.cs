0ac20c55760e74e28f13909c07da22bbfae163ad
DeukYu <ldy8070@naver.com>
2022-06-18 14:49:56 +0900

    Scene Manager
    Scene Manager

--- Client/Assets/Scripts/Controllers/PlayerController.cs
+++ Client/Assets/Scripts/Controllers/PlayerController.cs
@@ -14,9 +14,6 @@ public class PlayerController : MonoBehaviour
     {
         Managers.Input.MouseAction -= OnMouseClicked;
         Managers.Input.MouseAction += OnMouseClicked;
-
-        // TEMP
-        Managers.UI.ShowSceneUI<UI_Inven>();
     }
 
     public enum PlayerState
--- Client/Assets/Scripts/Managers/Managers.cs
+++ Client/Assets/Scripts/Managers/Managers.cs
@@ -9,9 +9,11 @@ public class Managers : MonoBehaviour
 
     InputManager _input = new InputManager();
     ResourceManager _resource = new ResourceManager();
+    SceneManagerEx _scene = new SceneManagerEx();
     UIManager _ui = new UIManager();
     public static InputManager Input { get { return Instance._input; } }
     public static ResourceManager Resource { get { return Instance._resource; } }
+    public static SceneManagerEx Scene { get { return Instance._scene; } }
     public static UIManager UI { get { return Instance._ui; } }
 
     void Start()
@@ -29,7 +31,7 @@ public class Managers : MonoBehaviour
         if (s_instance == null)
         {
             GameObject go = GameObject.Find("@Managers");
-            if(go != null)
+            if(go == null)
             {
                 go = new GameObject { name = "@Managers" };
                 go.AddComponent<Managers>();
