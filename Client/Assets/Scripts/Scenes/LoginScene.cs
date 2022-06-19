54baed76f5deaba87c590f7656f494143d0cb334
DeukYu <ldy8070@naver.com>
2022-06-20 00:06:40 +0900

    PoolManager
    PoolManager

--- Client/Assets/Scripts/Scenes/LoginScene.cs
+++ Client/Assets/Scripts/Scenes/LoginScene.cs
@@ -10,6 +10,17 @@ public class LoginScene : BaseScene
         base.Init();
 
         SceneType = Define.Scene.Login;
+
+        List<GameObject> list = new List<GameObject>();
+        for(int i=0;i < 2; i++)
+        {
+            Managers.Resource.Instantiate("UnityChan");
+        }
+
+        foreach(GameObject obj in list)
+        {
+            Managers.Resource.Destroy(obj);
+        }
     }
     private void Update()
     {
