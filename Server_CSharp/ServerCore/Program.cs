efeb682f394eda66ec8d66de6336bf68d2a9e524
DeukYu <ldy8070@naver.com>
2022-07-04 23:28:29 +0900

    Thread
    Thread

--- Server_CSharp/ServerCore/Program.cs
+++ Server_CSharp/ServerCore/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace ServerCore
+{
+    class Program
+    {
+        static void MainThread(object state)
+        {
+            Console.WriteLine($"{state}");
+        }
+
+        static void Main(string[] args)
+        {
+            ThreadPool.QueueUserWorkItem(MainThread, "Hello from the thread pool.");
+            ThreadPool.backgra
+            Console.WriteLine("Main thread does some work, then sleeps.");
+
+            ThreadPool.
+            Console.WriteLine("Main thread exits.");
+        }
+    }
+}
