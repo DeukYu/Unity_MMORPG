eb592c2440a914cb1082c5fdfce45f81bc1dc2e3
DeukYu <ldy8070@naver.com>
2022-07-14 23:11:54 +0900

    메모리 베리어
    메모리 베리어

--- Server_CSharp/ServerCore/Program.cs
+++ Server_CSharp/ServerCore/Program.cs
@@ -3,19 +3,49 @@ namespace ServerCore
 {
     class Program
     {
-        static void MainThread(object state)
+        static int x = 0;
+        static int y = 0;
+        static int r1 = 0;
+        static int r2 = 0;
+
+        static void Thread_1()
         {
-            Console.WriteLine($"{state}");
+            y = 1; // Store 
+            Thread.MemoryBarrier(); // 메모리 배리어
+            r1 = x; // Load
+        }
+
+        static void Thread_2()
+        {
+            x = 1; // Store
+            Thread.MemoryBarrier(); // 메모리 배리어
+            r2 = y; // Load 
         }
 
         static void Main(string[] args)
         {
-            ThreadPool.QueueUserWorkItem(MainThread, "Hello from the thread pool.");
-            ThreadPool.backgra
-            Console.WriteLine("Main thread does some work, then sleeps.");
+            int count = 0;
 
-            ThreadPool.
-            Console.WriteLine("Main thread exits.");
+            while (true)
+            {
+                x = y = r1 = r2 = 0;
+                count++;
+
+                Task t1 = new Task(Thread_1);
+                Task t2 = new Task(Thread_2);
+
+                t1.Start();
+                t2.Start();
+
+                Task.WaitAll(t1, t2); // 제공된 모든 Task 개체의 실행이 완료되기를 기다립니다.
+
+                if (r1 == 0 && r2 == 0)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine($"{count}번 만에 빠져 나옴");
         }
     }
 }
