0b3d0f6416e6923a1a4df160d2ec3338d492e450
DeukYu <ldy8070@naver.com>
2022-07-19 18:19:01 +0900

    WriteReadLock 구현
    WriteReadLock 구현

--- Server_CSharp/ServerCore/Program.cs
+++ Server_CSharp/ServerCore/Program.cs
@@ -3,62 +3,35 @@ using System.Threading;
 
 namespace ServerCore
 {
-    class SpinLock
-    {
-        volatile int _locked = 0;
-
-        public void Acquire()
-        {
-            while(true)
-            {
-                //int original = Interlocked.Exchange(ref _locked, 1);
-                //if (original == 0)
-                //    break;
-                // CAS Compare-And-Swap
-                int expected = 0;
-                int desired = 1;
-                if (Interlocked.CompareExchange(ref _locked, desired, expected) == expected)
-                    break;
-            }
-        }
-        public void Release()
-        {
-            _locked = 0;
-        }
-    }
     class Program
     {
-        static int _num = 0;
-        static SpinLock _lock = new SpinLock();
-
-        static void Thread_1()
-        {
-            for(int i = 0; i<100000; ++i)
-            {
-                _lock.Acquire();
-                _num++;
-                _lock.Release();
-            }
-        }
-        static void Thread_2()
-        {
-            for (int i = 0; i < 100000; ++i)
-            {
-                _lock.Acquire();
-                _num--;
-                _lock.Release();
-            }
-        }
+        static volatile int count = 0;
+        static Lock _lock = new Lock();
         static void Main(string[] args)
         {
-            Task t1 = new Task(Thread_1);
-            Task t2 = new Task(Thread_2);
+            Task t1 = new Task(delegate ()
+            {
+                for (int i = 0; i < 10000; ++i)
+                {
+                    _lock.WriteLock();
+                    count++;
+                    _lock.WriteUnlock();
+                }
+            });
+            Task t2 = new Task(delegate ()
+            {
+                for (int i = 0; i < 10000; ++i)
+                {
+                    _lock.WriteLock();
+                    count--;
+                    _lock.WriteUnlock();
+                }
+            });
+
             t1.Start();
             t2.Start();
-
             Task.WaitAll(t1, t2);
-
-            Console.WriteLine(_num);
+            Console.WriteLine(count);
         }
     }
 }
