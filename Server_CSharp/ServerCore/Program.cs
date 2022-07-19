df60437a07696ca87dad096e3b2c43077809b097
DeukYu <ldy8070@naver.com>
2022-07-19 16:15:09 +0900

    SpinLock 구현
    SpinLock 구현

--- Server_CSharp/ServerCore/Program.cs
+++ Server_CSharp/ServerCore/Program.cs
@@ -1,51 +1,64 @@
-﻿
+﻿using System;
+using System.Threading;
+
 namespace ServerCore
 {
+    class SpinLock
+    {
+        volatile int _locked = 0;
+
+        public void Acquire()
+        {
+            while(true)
+            {
+                //int original = Interlocked.Exchange(ref _locked, 1);
+                //if (original == 0)
+                //    break;
+                // CAS Compare-And-Swap
+                int expected = 0;
+                int desired = 1;
+                if (Interlocked.CompareExchange(ref _locked, desired, expected) == expected)
+                    break;
+            }
+        }
+        public void Release()
+        {
+            _locked = 0;
+        }
+    }
     class Program
     {
-        static int x = 0;
-        static int y = 0;
-        static int r1 = 0;
-        static int r2 = 0;
+        static int _num = 0;
+        static SpinLock _lock = new SpinLock();
 
         static void Thread_1()
         {
-            y = 1; // Store 
-            Thread.MemoryBarrier(); // 메모리 배리어
-            r1 = x; // Load
+            for(int i = 0; i<100000; ++i)
+            {
+                _lock.Acquire();
+                _num++;
+                _lock.Release();
+            }
         }
-
         static void Thread_2()
         {
-            x = 1; // Store
-            Thread.MemoryBarrier(); // 메모리 배리어
-            r2 = y; // Load 
+            for (int i = 0; i < 100000; ++i)
+            {
+                _lock.Acquire();
+                _num--;
+                _lock.Release();
+            }
         }
-
         static void Main(string[] args)
         {
-            int count = 0;
+            Task t1 = new Task(Thread_1);
+            Task t2 = new Task(Thread_2);
+            t1.Start();
+            t2.Start();
 
-            while (true)
-            {
-                x = y = r1 = r2 = 0;
-                count++;
+            Task.WaitAll(t1, t2);
 
-                Task t1 = new Task(Thread_1);
-                Task t2 = new Task(Thread_2);
-
-                t1.Start();
-                t2.Start();
-
-                Task.WaitAll(t1, t2); // 제공된 모든 Task 개체의 실행이 완료되기를 기다립니다.
-
-                if (r1 == 0 && r2 == 0)
-                {
-                    break;
-                }
-            }
-
-            Console.WriteLine($"{count}번 만에 빠져 나옴");
+            Console.WriteLine(_num);
         }
     }
 }
