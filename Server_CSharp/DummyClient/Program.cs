62ad877e105177e885f15f3b4f84eb33bcf73ea7
DeukYu <ldy8070@naver.com>
2022-07-28 22:52:57 +0900

    ServerCore, Connector
    ServerCore, Connector

--- Server_CSharp/DummyClient/Program.cs
+++ Server_CSharp/DummyClient/Program.cs
@@ -2,11 +2,43 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
+using ServerCore;
+
 
 namespace DummyClient
 {
     class Program
     {
+        class GameSession : Session
+        {
+            public override void OnConnected(EndPoint endPoint)
+            {
+                Console.WriteLine($"OnConnected : {endPoint}");
+
+                for (int i = 0; i < 5; ++i)
+                {
+                    byte[] sendBuff = Encoding.UTF8.GetBytes($"Hello World!{i}");
+                    Send(sendBuff);
+                }
+            }
+
+            public override void OnDisconnected(EndPoint endPoint)
+            {
+                Console.WriteLine($"OnDisconnected : {endPoint}");
+            }
+
+            public override void OnRecv(ArraySegment<byte> buffer)
+            {
+                string recvData = Encoding.UTF8.GetString(buffer.Array, buffer.Offset, buffer.Count);
+                Console.WriteLine($"[From Server] {recvData}");
+            }
+
+            public override void OnSend(int numOfBytes)
+            {
+                Console.WriteLine($"Transferred byte : {numOfBytes}");
+            }
+        }
         static void Main(string[] args)
         {
             // DNS (Domain Name System)
@@ -15,28 +47,14 @@ namespace DummyClient
             IPAddress ipAddr = iPHost.AddressList[0];
             IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
+            Connector connector = new Connector();
+            connector.Connect(endPoint, () => { return new GameSession(); });
+
             while(true)
             {
-                Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-
                 try
                 {
-                    socket.Connect(endPoint);
-                    Console.WriteLine($"Connected To {socket.RemoteEndPoint.ToString()}");
-
-                    for(int i = 0; i < 5; ++i)
-                    {
-                        byte[] sendBuff = Encoding.UTF8.GetBytes($"Hello World!{i}");
-                        int sendBytes = socket.Send(sendBuff);
-                    }
-
-                    byte[] recvBuff = new byte[1024];
-                    int recvBytes = socket.Receive(recvBuff);
-                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
-                    Console.WriteLine($"[From Server] {recvData}");
-
-                    socket.Shutdown(SocketShutdown.Both);
-                    socket.Close();
+                   
                 }
                 catch (Exception e)
                 {
--- Server_CSharp/ServerCore/Session.cs
+++ Server_CSharp/ServerCore/Session.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
-    abstract class Session
+    public abstract class Session
     {
         Socket _socket;
         int _disconnected = 0;
