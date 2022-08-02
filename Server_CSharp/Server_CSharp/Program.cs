899ef4fc249911fcdeb7974a198f566a957d5cd1
DeukYu <ldy8070@naver.com>
2022-08-02 23:38:17 +0900

    SendBuffer
    SendBuffer

--- Server_CSharp/Server_CSharp/Program.cs
+++ Server_CSharp/Server_CSharp/Program.cs
@@ -7,13 +7,28 @@ using ServerCore;
 
 namespace Server
 {
+    class Knight
+    {
+        public int hp;
+        public int attack;
+        public string name;
+        public List<int> skills = new List<int>();
+    }
     class GameSession : Session
     {
         public override void OnConnected(EndPoint endPoint)
         {
             Console.WriteLine($"OnConnected : {endPoint}");
 
-            byte[] sendBuff = Encoding.UTF8.GetBytes("Weclcome to MMORPG Server !");
+            Knight knight = new Knight() { hp = 100, attack = 10 };
+           
+            ArraySegment<byte> openSegment = SendBufferHelper.Open(4096);
+            byte[] buffer = BitConverter.GetBytes(knight.hp);
+            byte[] buffer2 = BitConverter.GetBytes(knight.attack);
+            Array.Copy(buffer, 0, openSegment.Array, openSegment.Offset, buffer.Length);
+            Array.Copy(buffer2, 0, openSegment.Array, openSegment.Offset + buffer.Length, buffer2.Length);
+            ArraySegment<byte> sendBuff = SendBufferHelper.Close(buffer.Length + buffer2.Length);
+
             Send(sendBuff);
             Thread.Sleep(1000);
             Disconnect();
