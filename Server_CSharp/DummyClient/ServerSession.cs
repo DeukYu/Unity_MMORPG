bb6bb2f324bce098e8590bf57f0cc1a31d431130
DeukYu <ldy8070@naver.com>
2022-08-04 22:30:28 +0900

    Serialization
    Serialization

---
+++ Server_CSharp/DummyClient/ServerSession.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using ServerCore;
+
+namespace DummyClient
+{
+    class Packet
+    {
+        public ushort size;
+        public ushort packetId;
+    }
+
+    class PlayerInfoReq : Packet
+    {
+        public long playerId;
+    }
+    class PlayerInfoRes : Packet
+    {
+        public int hp;
+        public int attack;
+    }
+    public enum PacketID
+    {
+        PlayerInfoReq = 1,
+        PlayerInfoRes = 2,
+    }
+    class ServerSession : Session
+    {
+        public override void OnConnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnConnected : {endPoint}");
+
+            PlayerInfoReq packet = new PlayerInfoReq() { packetId = (ushort)PacketID.PlayerInfoReq, playerId = 1001 };
+
+            ArraySegment<byte> s = SendBufferHelper.Open(4096);
+            ushort count = 0;
+            bool bSuccess = true;
+
+            
+            count += 2;
+            bSuccess &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), packet.packetId);
+            count += 2;
+            bSuccess &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), packet.playerId);
+            count += 8;
+
+            bSuccess &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset, s.Count), count);
+
+            ArraySegment<byte> sendBuff = SendBufferHelper.Close(count);
+
+            if (bSuccess)
+                Send(sendBuff);
+        }
+
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnDisconnected : {endPoint}");
+        }
+
+        public override int OnRecv(ArraySegment<byte> buffer)
+        {
+            string recvData = Encoding.UTF8.GetString(buffer.Array, buffer.Offset, buffer.Count);
+            Console.WriteLine($"[From Server] {recvData}");
+            return buffer.Count;
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            Console.WriteLine($"Transferred byte : {numOfBytes}");
+        }
+    }
+}
--- Server_CSharp/Server_CSharp/Program.cs
+++ Server_CSharp/Server_CSharp/Program.cs
@@ -7,46 +7,6 @@ using ServerCore;
 
 namespace Server
 {
-    class Packet
-    {
-        public ushort size;
-        public ushort packetId;
-    }
-    class GameSession : PacketSession
-    {
-        public override void OnConnected(EndPoint endPoint)
-        {
-            Console.WriteLine($"OnConnected : {endPoint}");
-
-            //Packet packet = new Packet() { size = 100, packetId = 10 };
-           
-            //ArraySegment<byte> openSegment = SendBufferHelper.Open(4096);
-            //byte[] buffer = BitConverter.GetBytes(packet.size);
-            //byte[] buffer2 = BitConverter.GetBytes(packet.packetId);
-            //Array.Copy(buffer, 0, openSegment.Array, openSegment.Offset, buffer.Length);
-            //Array.Copy(buffer2, 0, openSegment.Array, openSegment.Offset + buffer.Length, buffer2.Length);
-            //ArraySegment<byte> sendBuff = SendBufferHelper.Close(buffer.Length + buffer2.Length);
-
-            //Send(sendBuff);
-            Thread.Sleep(5000);
-            Disconnect();
-        }
-        public override void OnRecvPacket(ArraySegment<byte> buffer)
-        {
-            ushort size = BitConverter.ToUInt16(buffer.Array, buffer.Offset);
-            ushort id = BitConverter.ToUInt16(buffer.Array, buffer.Offset + 2);
-            Console.WriteLine($"RecvPacketId: {id} Size: {size}");
-            throw new NotImplementedException();
-        }
-        public override void OnDisconnected(EndPoint endPoint)
-        {
-            Console.WriteLine($"OnDisconnected : {endPoint}");
-        }
-        public override void OnSend(int numOfBytes)
-        {
-            Console.WriteLine($"Transferred byte : {numOfBytes}");
-        }
-    }
     class Program
     {
         static Listener _listener = new Listener();
@@ -58,7 +18,7 @@ namespace Server
             IPAddress ipAddr = iPHost.AddressList[0];
             IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
-            _listener.Init(endPoint, () => { return new GameSession(); });
+            _listener.Init(endPoint, () => { return new ClientSession(); });
             Console.WriteLine("Listening...");
             while (true)
             {
